acca7bffc9e4dbbce3d74bb2f09a9fa574fab224
Hoon Lee <yl.hoony@gmail.com>
2018-03-29 03:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,20 +13,14 @@ function overwriteBestCustomer(customer) {
   bestCustomer = customer;
 }
 
-////////////////////////////////////
 const leastFavoriteCustomer = 'Billy';
 
 function reassignLeastFavorite() {
   leastFavoriteCustomer = 'Paul';
 }
 
-
-
 function attemptTwoFavoriteCustomers() {
   let favoriteCustomer = 'Colin';
   let favoriteCustomer = 'Jean';
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> cf86f6666dea2609712bf9b06e3b173ddf2bc7d7
